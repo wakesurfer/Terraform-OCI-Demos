3d8d2f0050bc9d9d86bf5e456838c784386cdcfe
wakesurfer <wakesurfer@users.noreply.github.com>
2018-09-28 16:17:14 +0200

    Add initial files

---
+++ ol7-compute-instance-w-bootvol/variables.tf
@@ -0,0 +1,39 @@
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "region" {}
+
+variable "compartment_ocid" {}
+variable "ssh_public_key" {}
+
+variable "pk_availability_domain_name" {}
+
+# Choose an Availability Domain
+variable "AD" {
+  default = "3"
+}
+
+variable "InstanceShape" {
+  default = "VM.Standard1.1"
+}
+
+variable "ImageOS" {
+  default = "Oracle Linux"
+}
+
+variable "ImageOSVersion" {
+  default = "7.5"
+}
+
+variable "instance_image_ocid" {
+  type = "map"
+
+  default = {
+    // See https://docs.us-phoenix-1.oraclecloud.com/images/
+    us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaasez4lk2lucxcm52nslj5nhkvbvjtfies4yopwoy4b3vysg5iwjra"
+    us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaa2tq67tvbeavcmioghquci6p3pvqwbneq3vfy7fe7m7geiga4cnxa"
+    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaakzrywmh7kwt7ugj5xqi5r4a7xoxsrxtc7nlsdyhmhqyp7ntobjwq"
+    uk-london-1    = "ocid1.image.oc1.uk-london-1.aaaaaaaalsdgd47nl5tgb55sihdpqmqu2sbvvccjs6tmbkr4nx2pq5gkn63a"
+  }
+}
