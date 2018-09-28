c7c372513ee049d32067802cc07cacbaa77d2045
wakesurfer <wakesurfer@users.noreply.github.com>
2018-09-28 17:48:08 +0200

    Change PATH to API key file from relative path to absolute path

--- ol7-compute-instance-w-bootvol/env-vars.sh
+++ ol7-compute-instance-w-bootvol/env-vars.sh
@@ -3,8 +3,7 @@
 export TF_VAR_tenancy_ocid="ocid1.tenancy.oc1..aaaaaaaajccl7okjwfkjp5t4dxdvzjyzixvpiyoo45xaxwtzl226czz6gfsq"
 export TF_VAR_user_ocid="ocid1.user.oc1..aaaaaaaac4qrtato4e7pufbkorxkgs6ueh7j3mbs237xhyy5fynon2xmmi4q"
 export TF_VAR_fingerprint="1d:3f:d1:6b:44:79:e5:2d:db:10:67:88:66:87:db:c1"
-#export TF_VAR_private_key_path="/mnt/c/Users/PEKARLSS/Documents/Oracle/Work/Terraform-OCI-Create-Linux-Compute/.oci/oci_api_key.pem"
-export TF_VAR_private_key_path="./.oci/oci_api_key.pem"
+export TF_VAR_private_key_path="/mnt/c/Users/PEKARLSS/Documents/GitHub/Terraform-OCI-Demos/ol7-compute-instance-w-bootvol/.oci/oci_api_key.pem"
 
 #Region
 export TF_VAR_region="eu-frankfurt-1"
