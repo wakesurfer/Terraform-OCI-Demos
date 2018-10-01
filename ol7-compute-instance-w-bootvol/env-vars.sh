efd2c3b1bbcac8b919b279b9910432d7190a61b8
wakesurfer <wakesurfer@users.noreply.github.com>
2018-10-01 11:16:21 +0200

    Cleanup in env_vars

--- ol7-compute-instance-w-bootvol/env-vars.sh
+++ ol7-compute-instance-w-bootvol/env-vars.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-#Authentication details
+### Authentication details
 export TF_VAR_tenancy_ocid="ocid1.tenancy.oc1..aaaaaaaajccl7okjwfkjp5t4dxdvzjyzixvpiyoo45xaxwtzl226czz6gfsq"
 export TF_VAR_user_ocid="ocid1.user.oc1..aaaaaaaac4qrtato4e7pufbkorxkgs6ueh7j3mbs237xhyy5fynon2xmmi4q"
 export TF_VAR_fingerprint="1d:3f:d1:6b:44:79:e5:2d:db:10:67:88:66:87:db:c1"
 export TF_VAR_private_key_path="/mnt/c/Users/PEKARLSS/Documents/GitHub/Terraform-OCI-Demos/ol7-compute-instance-w-bootvol/.oci/oci_api_key.pem"
 
-#Region
-#export TF_VAR_region="eu-frankfurt-1"
-export TF_VAR_region="us-ashburn-1"
-
-#Compartment
+### Compartment
 export TF_VAR_compartment_ocid="ocid1.compartment.oc1..aaaaaaaa4qkenymfoumszv5gs2am6rsciozq7zvlirhmfw2riwuurv45cwua"
 
+### Region and availability_domain
+#export TF_VAR_region="eu-frankfurt-1"
 #export TF_VAR_pk_availability_domain_name="RNnO:EU-FRANKFURT-1-AD-1"
+export TF_VAR_region="us-ashburn-1"
 export TF_VAR_pk_availability_domain_name="RNnO:US-ASHBURN-AD-1"
 
-#Public/private keys used on the instance
+### Public/private keys used on the instance
 export TF_VAR_ssh_public_key=$(cat /home/peter/.ssh/id_rsa.pub)
 export TF_VAR_ssh_private_key=$(cat /home/peter/.ssh/id_rsa)
